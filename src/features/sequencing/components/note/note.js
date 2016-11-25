24ba7e06ca15e85f35ac3aed29e025e5e55dfdf7
Nick Johnson <nickjohnson.dev@gmail.com>
2016-11-24 21:31:20 -0600

    Converted note to ES6 class

--- src/features/sequencing/components/note/note.js
+++ src/features/sequencing/components/note/note.js
@@ -1,102 +1,100 @@
 import _ from 'lodash';
-import { PropTypes } from 'react';
+import React from 'react';
 import h from 'react-hyperscript';
 import classnames from 'classnames';
-import { compose, mapProps, setDisplayName, setPropTypes, pure, withHandlers } from 'recompose';
 import './note.scss';
 
-const component = props => h('.note', {
-  className: props.className,
-  style: {
-    transform: props.transform,
-  },
-}, [
-  h('.note__point', {
-    onMouseDown: props.handleMouseDown,
-    onMouseUp: props.handleMouseUp,
-  }, [
-    h('.note__point__fill'),
-  ]),
-  h('.note__point-connector', {
-    style: {
-      transform: props.connectorTransform,
-    },
-  }),
-  h('.note__point', {
-    style: {
-      display: props.endPointDisplay,
-      transform: props.endPointTransform,
-    },
-    onMouseDown: props.handleEndpointMouseDown,
-  }, [
-    h('.note__point__fill'),
-  ]),
-]);
+export class Note extends React.Component {
+  static propTypes = {
+    isSelected: React.PropTypes.bool.isRequired,
+    note: React.PropTypes.object.isRequired,
+    onEndpointMouseDown: React.PropTypes.func,
+    onMouseDown: React.PropTypes.func,
+    onMouseUp: React.PropTypes.func,
+  }
 
-export const Note = compose(
-  setDisplayName('Note'),
-  pure,
-  setPropTypes({
-    note: PropTypes.object.isRequired,
-    isSelected: PropTypes.bool.isRequired,
-    onEndpointMouseDown: PropTypes.func,
-    onEndpointMouseUp: PropTypes.func,
-    onMouseDown: PropTypes.func,
-    onMouseUp: PropTypes.func,
-  }),
-  withHandlers({
-    handleMouseDown: ({ onMouseDown, note }) => (e) => {
-      if (!onMouseDown) return;
-      onMouseDown(note, e);
-    },
-    handleMouseUp: ({ onMouseUp, note }) => (e) => {
-      if (!onMouseUp) return;
-      onMouseUp(note, e);
-    },
-    handleEndpointMouseDown: props => (e) => {
-      if (!props.onEndpointMouseDown) return;
-      props.onEndpointMouseDown(props.note, e);
-    },
-    handleEndpointMouseUp: props => (e) => {
-      if (!props.onEndpointMouseUp) return;
-      props.onEndpointMouseUp(props.note, e);
-    },
-  }),
-  mapProps(props => ({
-    ...props,
-    className: classnames({ 'note--active': props.isSelected }),
-    connectorTransform: getConnectorTransform(
-      _.first(props.note.points),
-      _.last(props.note.points),
-    ),
-    endPointDisplay: is32ndNote(props.note) === 0 ? 'none' : 'flex',
-    endPointTransform: getEndPointTransform(_.first(props.note.points), _.last(props.note.points)),
-    transform: getTransform(props.note),
-  })),
-)(component);
+  render() {
+    return h('.note', {
+      className: this.getClassName(),
+      style: {
+        transform: this.getTransform(),
+      },
+    }, [
+      h('.note__point', {
+        onMouseDown: this.handleMouseDown,
+        onMouseUp: this.handleMouseUp,
+      }, [
+        h('.note__point__fill'),
+      ]),
+      h('.note__point-connector', {
+        style: {
+          transform: this.getConnectorTransform(),
+        },
+      }),
+      h('.note__point', {
+        style: {
+          display: this.getEndPointDisplay(),
+          transform: this.getEndPointTransform(),
+        },
+        onMouseDown: this.handleEndpointMouseDown,
+      }, [
+        h('.note__point__fill'),
+      ]),
+    ]);
+  }
 
-function getConnectorTransform(startPoint, endPoint) {
-  const { asin, abs, PI, sign, sqrt } = Math;
-  const x = ((endPoint.x - startPoint.x) * 40);
-  const y = (endPoint.y - startPoint.y) * 40;
-  const length = x !== 0
-    ? sqrt(abs((x ** 2) + (y ** 2)))
-    : 0;
-  const rotation = x !== 0
-    ? asin(abs(y / length)) * (180 / PI) * sign(y)
-    : 0;
-  return `rotate(${rotation}deg) scaleX(${x ? length : 0})`;
-}
+  getClassName() {
+    return classnames({
+      'note--active': this.props.isSelected,
+    });
+  }
 
-function getEndPointTransform(startPoint, endPoint) {
-  const x = (endPoint.x - startPoint.x) * 40;
-  const y = (endPoint.y - startPoint.y) * 40;
-  return `translate(${x}px, ${y}px)`;
-}
+  getConnectorTransform() {
+    const startPoint = _.first(this.props.note.points);
+    const endPoint = _.last(this.props.note.points);
+    const { asin, abs, PI, sign, sqrt } = Math;
+    const x = ((endPoint.x - startPoint.x) * 40);
+    const y = (endPoint.y - startPoint.y) * 40;
+    const length = x !== 0
+      ? sqrt(abs((x ** 2) + (y ** 2)))
+      : 0;
+    const rotation = x !== 0
+      ? asin(abs(y / length)) * (180 / PI) * sign(y)
+      : 0;
+    return `rotate(${rotation}deg) scaleX(${x ? length : 0})`;
+  }
 
-function getTransform(note) {
-  const { x, y } = _.first(note.points);
-  return `translate(${x * 40}px, ${y * 40}px)`;
+  getEndPointDisplay() {
+    return is32ndNote(this.props.note) === 0 ? 'none' : 'flex';
+  }
+
+  getEndPointTransform() {
+    const startPoint = _.first(this.props.note.points);
+    const endPoint = _.last(this.props.note.points);
+    const x = (endPoint.x - startPoint.x) * 40;
+    const y = (endPoint.y - startPoint.y) * 40;
+    return `translate(${x}px, ${y}px)`;
+  }
+
+  getTransform() {
+    const { x, y } = _.first(this.props.note.points);
+    return `translate(${x * 40}px, ${y * 40}px)`;
+  }
+
+  handleEndpointMouseDown = (e) => {
+    if (!this.props.onEndpointMouseDown) return;
+    this.props.onEndpointMouseDown(this.props.note, e);
+  }
+
+  handleMouseDown = (e) => {
+    if (!this.props.onMouseDown) return;
+    this.props.onMouseDown(this.props.note, e);
+  }
+
+  handleMouseUp = (e) => {
+    if (!this.props.onMouseUp) return;
+    this.props.onMouseUp(this.props.note, e);
+  }
 }
 
 function is32ndNote(note) {
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
 import { applyMiddleware, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import loggerMiddleware from './logger-middleware';
+// import loggerMiddleware from './logger-middleware';
 import reducer from './reducer';
 import saga from './saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const middleware = applyMiddleware(
-  loggerMiddleware,
-  sagaMiddleware
+  // loggerMiddleware,
+  sagaMiddleware,
 );
 
 const store = createStore(reducer, middleware);
