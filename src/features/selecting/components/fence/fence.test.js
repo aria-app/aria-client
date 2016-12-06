17a5c05cf20971d6f4d4ef306f271d22c7ac578f
Nick Johnson <nickjohnson.dev@gmail.com>
2016-12-06 17:15:23 -0600

    Filled out fence tests

--- src/features/selecting/components/fence/fence.test.js
+++ src/features/selecting/components/fence/fence.test.js
@@ -5,22 +5,128 @@ import { Fence } from './fence';
 describe('Fence Component', () => {
   it('should be defined', () => {
     const component = shallow(h(Fence, {
-      endPoint: { x: 0, y: 0 },
-      isSelecting: false,
-      startPoint: { x: 0, y: 0 },
+      ...getDefaultProps(),
     }));
     expect(component.length).toEqual(1);
   });
 
-  it('should have display "block" when selecting and has moved from start point');
-  it('should have display "none" when still at start point');
-  it('should have display "none" when not selecting');
-  it('should have width of (change in x) * 40');
-  it('should have width of 0 when start point is empty');
-  it('should have width of 0 when end point is empty');
-  it('should have height of (change in y) * 40');
-  it('should have height of 0 when start point is empty');
-  it('should have height of 0 when end point is empty');
-  it('should be translated to top left corner of selection when growing normally');
-  it('should be translated to top left corner of selection when growing inversely');
+  it('should have display "block" when selecting and has moved from start point', () => {
+    const component = shallow(h(Fence, {
+      ...getDefaultProps(),
+      endPoint: { x: 0, y: 1 },
+      isSelecting: true,
+      startPoint: { x: 0, y: 0 },
+    }));
+    expect(component.prop('style').display).toEqual('block');
+  });
+
+  it('should have display "none" when still at start point', () => {
+    const component = shallow(h(Fence, {
+      ...getDefaultProps(),
+      endPoint: { x: 0, y: 0 },
+      isSelecting: true,
+      startPoint: { x: 0, y: 0 },
+    }));
+    expect(component.prop('style').display).toEqual('none');
+  });
+
+  it('should have display "none" when not selecting', () => {
+    const component = shallow(h(Fence, {
+      ...getDefaultProps(),
+      endPoint: { x: 0, y: 1 },
+      isSelecting: false,
+      startPoint: { x: 0, y: 0 },
+    }));
+    expect(component.prop('style').display).toEqual('none');
+  });
+
+  it('should have width of (change in x + 1) * 40', () => {
+    const component = shallow(h(Fence, {
+      ...getDefaultProps(),
+      endPoint: { x: 50, y: 0 },
+      isSelecting: false,
+      startPoint: { x: 0, y: 0 },
+    }));
+    expect(component.prop('style').width).toEqual(51 * 40);
+  });
+
+  it('should have width of 0 when start point is empty', () => {
+    const component = shallow(h(Fence, {
+      ...getDefaultProps(),
+      endPoint: { x: 0, y: 0 },
+      isSelecting: false,
+      startPoint: {},
+    }));
+    expect(component.prop('style').width).toEqual(0);
+  });
+
+  it('should have width of 0 when end point is empty', () => {
+    const component = shallow(h(Fence, {
+      ...getDefaultProps(),
+      endPoint: {},
+      isSelecting: false,
+      startPoint: { x: 0, y: 0 },
+    }));
+    expect(component.prop('style').width).toEqual(0);
+  });
+
+  it('should have height of (change in y + 1) * 40', () => {
+    const component = shallow(h(Fence, {
+      ...getDefaultProps(),
+      endPoint: { x: 0, y: 40 },
+      isSelecting: false,
+      startPoint: { x: 0, y: 0 },
+    }));
+    expect(component.prop('style').height).toEqual(41 * 40);
+  });
+
+  it('should have height of 0 when start point is empty', () => {
+    const component = shallow(h(Fence, {
+      ...getDefaultProps(),
+      endPoint: { x: 0, y: 0 },
+      isSelecting: false,
+      startPoint: {},
+    }));
+    expect(component.prop('style').height).toEqual(0);
+  });
+
+  it('should have height of 0 when end point is empty', () => {
+    const component = shallow(h(Fence, {
+      ...getDefaultProps(),
+      endPoint: {},
+      isSelecting: false,
+      startPoint: { x: 0, y: 0 },
+    }));
+    expect(component.prop('style').height).toEqual(0);
+  });
+
+  it('should be translated to top left corner of selection when growing normally', () => {
+    const component = shallow(h(Fence, {
+      ...getDefaultProps(),
+      endPoint: { x: 23, y: 45 },
+      isSelecting: true,
+      startPoint: { x: 10, y: 30 },
+    }));
+    const expected = 'translate(400px, 1200px)';
+    expect(component.prop('style').transform).toEqual(expected);
+  });
+
+  it('should be translated to top left corner of selection when growing inversely', () => {
+    const component = shallow(h(Fence, {
+      ...getDefaultProps(),
+      endPoint: { x: 4, y: 20 },
+      isSelecting: true,
+      startPoint: { x: 10, y: 30 },
+    }));
+    const expected = 'translate(160px, 800px)';
+    expect(component.prop('style').transform).toEqual(expected);
+  });
 });
+
+function getDefaultProps() {
+  return {
+    endPoint: { x: 0, y: 0 },
+    isSelecting: false,
+    startPoint: { x: 0, y: 0 },
+  };
+}
