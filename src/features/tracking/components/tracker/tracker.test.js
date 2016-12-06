032c1e401d733c6320df1e74a681b48dfc88eb85
Nick Johnson <nickjohnson.dev@gmail.com>
2016-12-05 21:39:00 -0600

    Filled out tests for tracker and song-toolbar

--- src/features/tracking/components/tracker/tracker.test.js
+++ src/features/tracking/components/tracker/tracker.test.js
@@ -1,5 +1,9 @@
 import h from 'react-hyperscript';
 import { shallow } from 'enzyme';
+import { SongTimelineContainer } from '../song-timeline-container/song-timeline-container';
+import { TrackEditingModalContainer } from '../track-editing-modal/track-editing-modal-container';
+import { TrackerToolbarContainer } from '../tracker-toolbar/tracker-toolbar-container';
+import { TracksContainer } from '../tracks/tracks-container';
 import { Tracker } from './tracker';
 
 describe('Tracker Component', () => {
@@ -7,16 +11,36 @@ describe('Tracker Component', () => {
     const component = shallow(h(Tracker));
     expect(component.length).toEqual(1);
   });
+
   describe('child component TrackerToolbarContainer', () => {
-    it('should be defined');
+    it('should be defined', () => {
+      const component = shallow(h(Tracker));
+      const trackerToolbarContainerEl = component.find(TrackerToolbarContainer);
+      expect(trackerToolbarContainerEl.length).toEqual(1);
+    });
   });
+
   describe('child component TracksContainer', () => {
-    it('should be defined');
+    it('should be defined', () => {
+      const component = shallow(h(Tracker));
+      const tracksContainerEl = component.find(TracksContainer);
+      expect(tracksContainerEl.length).toEqual(1);
+    });
   });
+
   describe('child component SongTimelineContainer', () => {
-    it('should be defined');
+    it('should be defined', () => {
+      const component = shallow(h(Tracker));
+      const songTimelineContainerEl = component.find(SongTimelineContainer);
+      expect(songTimelineContainerEl.length).toEqual(1);
+    });
   });
+
   describe('child component TrackEditingModalContainer', () => {
-    it('should be defined');
+    it('should be defined', () => {
+      const component = shallow(h(Tracker));
+      const trackEditingModalContainerEl = component.find(TrackEditingModalContainer);
+      expect(trackEditingModalContainerEl.length).toEqual(1);
+    });
   });
 });
