c29ef5faab6fef919525b3d91535b5fc71a5267c
Nick Johnson <nickjohnson.dev@gmail.com>
2016-12-05 22:29:03 -0600

    Filled out tests for upload overlay and context menu.

--- src/features/app/components/upload-overlay/upload-overlay.test.js
+++ src/features/app/components/upload-overlay/upload-overlay.test.js
@@ -1,25 +1,113 @@
+import _ from 'lodash';
 import h from 'react-hyperscript';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import { UploadOverlay } from './upload-overlay';
 
 describe('UploadOverlay Component', () => {
   it('should be defined', () => {
     const component = shallow(h(UploadOverlay, {
-      isFileOver: false,
-      onFileDragCancel: () => {},
-      onFileDrop: () => {},
+      ...getDefaultProps(),
     }));
     expect(component.length).toEqual(1);
   });
-  it('should have defined content when file is over');
-  it('should not have defined content when file is not over');
-  it('should invoke file drag cancel event when dragged file leaves');
-  it('should invoke file drop event with files when files are dropped');
-  it('should invoke file drag cancel event when dropped files are empty');
-  describe('element __tint', () => {
-    it('should be defined');
+
+  it('should have defined content when file is over', () => {
+    const component = shallow(h(UploadOverlay, {
+      ...getDefaultProps(),
+      isFileOver: true,
+    }));
+    const uploadOverlayEl = component.find('.upload-overlay');
+    expect(uploadOverlayEl.length).toEqual(1);
   });
+
+  it('should not have defined content when file is not over', () => {
+    const component = shallow(h(UploadOverlay, {
+      ...getDefaultProps(),
+      isFileOver: false,
+    }));
+    const uploadOverlayEl = component.find('.upload-overlay');
+    expect(uploadOverlayEl.length).toEqual(0);
+  });
+
+  it('should invoke file drag cancel event when dragged file leaves', () => {
+    const onFileDragCancel = sinon.spy();
+    const component = shallow(h(UploadOverlay, {
+      ...getDefaultProps(),
+      isFileOver: true,
+      onFileDragCancel,
+    }));
+    component.simulate('dragleave');
+    expect(onFileDragCancel.called).toEqual(true);
+  });
+
+  it('should invoke file drop event with file when files are dropped', () => {
+    const onFileDrop = sinon.spy();
+    const component = shallow(h(UploadOverlay, {
+      ...getDefaultProps(),
+      isFileOver: true,
+      onFileDrop,
+    }));
+    const files = [
+      { content: '{ "id": "my-song" }' },
+    ];
+    const e = {
+      dataTransfer: {
+        files,
+      },
+      preventDefault: () => {},
+      stopPropagation: () => {},
+    };
+    component.simulate('drop', e);
+    expect(_.last(onFileDrop.args)[0]).toEqual(files[0]);
+  });
+
+  it('should invoke file drag cancel event when dropped files are empty', () => {
+    const onFileDragCancel = sinon.spy();
+    const component = shallow(h(UploadOverlay, {
+      ...getDefaultProps(),
+      isFileOver: true,
+      onFileDragCancel,
+    }));
+    const files = [];
+    const e = {
+      dataTransfer: {
+        files,
+      },
+      preventDefault: () => {},
+      stopPropagation: () => {},
+    };
+    component.simulate('drop', e);
+    expect(onFileDragCancel.called).toEqual(true);
+  });
+
+  describe('element __tint', () => {
+    it('should be defined', () => {
+      const component = shallow(h(UploadOverlay, {
+        ...getDefaultProps(),
+        isFileOver: true,
+      }));
+      const tintEl = component.find('.upload-overlay__tint');
+      expect(tintEl.length).toEqual(1);
+    });
+  });
+
   describe('element __tint__drag-indicator', () => {
-    it('should be defined');
+    it('should be defined', () => {
+      const component = shallow(h(UploadOverlay, {
+        ...getDefaultProps(),
+        isFileOver: true,
+      }));
+      const tintDragIndicatorEl = component.find('.upload-overlay__tint__drag-indicator');
+      expect(tintDragIndicatorEl.length).toEqual(1);
+    });
   });
 });
+
+function getDefaultProps() {
+  return {
+    isFileOver: false,
+    onFileDragCancel: () => {},
+    onFileDrop: () => {},
+  };
+}
