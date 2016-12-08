84714648a7206fe83e5ef19be8505c17ea0ce406
Nick Johnson <nickjohnson.dev@gmail.com>
2016-12-07 21:26:57 -0600

    Filled out tests for keys and note

--- src/features/sequencing/components/keys/keys.test.js
+++ src/features/sequencing/components/keys/keys.test.js
@@ -1,6 +1,11 @@
+import _ from 'lodash';
 import h from 'react-hyperscript';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import shared from '../../../shared';
 import { Keys } from './keys';
+
+const { scale } = shared.constants;
 
 describe('Keys Component', () => {
   it('should be defined', () => {
@@ -9,13 +14,53 @@ describe('Keys Component', () => {
     }));
     expect(component.length).toEqual(1);
   });
+
   describe('element __key', () => {
-    it('should be defined once for each step in scale');
-    it('should have class name corresponding to step');
-    it('should invoke note preview event with corresponding step when clicked');
+    it('should be defined once for each step in scale', () => {
+      const component = shallow(h(Keys, {
+        onNotePreview: () => {},
+      }));
+      const keyEls = component.find('.keys__key');
+      expect(keyEls.length).toEqual(scale.length);
+    });
+
+    it('should have class name corresponding to step', () => {
+      const component = shallow(h(Keys, {
+        onNotePreview: () => {},
+      }));
+      const keyEls = component.find('.keys__key');
+      expect(keyEls.first().prop('className')).toContain('keys__key--b');
+    });
+
+    it('should invoke note preview event with corresponding step on mouse up', () => {
+      const onNotePreview = sinon.spy();
+      const component = shallow(h(Keys, {
+        onNotePreview,
+      }));
+      const keyEls = component.find('.keys__key');
+      const expected = {
+        y: scale[0].y,
+      };
+      keyEls.first().simulate('mouseup');
+      expect(_.last(onNotePreview.args)[0]).toEqual(expected);
+    });
   });
+
   describe('element __key__label', () => {
-    it('should be defined');
-    it('should contain step name');
+    it('should be defined once for each step in scale', () => {
+      const component = shallow(h(Keys, {
+        onNotePreview: () => {},
+      }));
+      const keyLabelEls = component.find('.keys__key__label');
+      expect(keyLabelEls.length).toEqual(scale.length);
+    });
+
+    it('should contain step name', () => {
+      const component = shallow(h(Keys, {
+        onNotePreview: () => {},
+      }));
+      const keyLabelEls = component.find('.keys__key__label');
+      expect(keyLabelEls.first().text()).toEqual(scale[0].name);
+    });
   });
 });
--- src/features/sequencing/components/note/note.test.js
+++ src/features/sequencing/components/note/note.test.js
@@ -1,49 +1,257 @@
+import _ from 'lodash';
 import h from 'react-hyperscript';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import { Note } from './note';
 
 describe('Note Component', () => {
   it('should be defined', () => {
     const component = shallow(h(Note, {
-      isSelected: false,
-      note: {
-        points: [
-          { x: 0, y: 0 },
-          { x: 1, y: 0 },
-        ],
-      },
-      onEndpointMouseDown: () => {},
-      onMouseDown: () => {},
-      onMouseUp: () => {},
+      ...getRequiredProps(),
     }));
     expect(component.length).toEqual(1);
   });
-  it('should have active class when active');
-  it('should not have active class when not active');
-  it('should have translate corresponding to note points');
+
+  it('should have active class when selected', () => {
+    const component = shallow(h(Note, {
+      ...getRequiredProps(),
+      isSelected: true,
+    }));
+    expect(component.prop('className')).toContain('note--active');
+  });
+
+  it('should not have active class when not selected', () => {
+    const component = shallow(h(Note, {
+      ...getRequiredProps(),
+      isSelected: false,
+    }));
+    expect(component.prop('className')).not.toContain('note--active');
+  });
+
+  it('should have translate corresponding to note points', () => {
+    const component = shallow(h(Note, {
+      ...getRequiredProps(),
+      note: {
+        points: [
+          { x: 10, y: 23 },
+          { x: 12, y: 23 },
+        ],
+      },
+    }));
+    const expected = 'translate(400px, 920px)';
+    expect(component.prop('style').transform).toEqual(expected);
+  });
+
   describe('element __point--start', () => {
-    it('should be defined');
-    it('should invoke mouse down with note and event when mouse down occurs');
-    it('should invoke mouse up with note and event when mouse up occurs');
+    it('should be defined', () => {
+      const component = shallow(h(Note, {
+        ...getRequiredProps(),
+      }));
+      const pointStartEl = component.find('.note__point--start');
+      expect(pointStartEl.length).toEqual(1);
+    });
+
+    it('should invoke mouse down with note and event when mouse down occurs', () => {
+      const note = {
+        points: [
+          { x: 10, y: 23 },
+          { x: 12, y: 23 },
+        ],
+      };
+      const onMouseDown = sinon.spy();
+      const component = shallow(h(Note, {
+        ...getRequiredProps(),
+        note,
+        onMouseDown,
+      }));
+      const pointStartEl = component.find('.note__point--start');
+      const e = {};
+      pointStartEl.simulate('mousedown', e);
+      expect(_.last(onMouseDown.args)[0]).toEqual(note);
+      expect(_.last(onMouseDown.args)[1]).toEqual(e);
+    });
+
+    it('should invoke mouse up with note and event when mouse up occurs', () => {
+      const note = {
+        points: [
+          { x: 10, y: 23 },
+          { x: 12, y: 23 },
+        ],
+      };
+      const onMouseUp = sinon.spy();
+      const component = shallow(h(Note, {
+        ...getRequiredProps(),
+        note,
+        onMouseUp,
+      }));
+      const pointStartEl = component.find('.note__point--start');
+      const e = {};
+      pointStartEl.simulate('mouseup', e);
+      expect(_.last(onMouseUp.args)[0]).toEqual(note);
+      expect(_.last(onMouseUp.args)[1]).toEqual(e);
+    });
   });
+
   describe('element __point__fill--start', () => {
-    it('should be defined');
+    it('should be defined', () => {
+      const component = shallow(h(Note, {
+        ...getRequiredProps(),
+      }));
+      const pointFillStartEl = component.find('.note__point__fill--start');
+      expect(pointFillStartEl.length).toEqual(1);
+    });
   });
+
   describe('element __point-connector', () => {
-    it('should be defined');
-    it('should have correct transform applied when the note when the note is straight');
-    it('should have correct transform applied when the note when the note is bent');
-    it('should have correct transform applied when the note is a 32nd note');
+    it('should be defined', () => {
+      const component = shallow(h(Note, {
+        ...getRequiredProps(),
+      }));
+      const pointConnectorEl = component.find('.note__point-connector');
+      expect(pointConnectorEl.length).toEqual(1);
+    });
+
+    it('should have correct transform applied when the note when the note is straight', () => {
+      const component = shallow(h(Note, {
+        ...getRequiredProps(),
+        note: {
+          points: [
+            { x: 0, y: 35 },
+            { x: 1, y: 35 },
+          ],
+        },
+      }));
+      const pointConnectorEl = component.find('.note__point-connector');
+      const expected = 'rotate(0deg) scaleX(40)';
+      expect(pointConnectorEl.prop('style').transform).toEqual(expected);
+    });
+
+    it('should have correct transform applied when the note when the note is bent', () => {
+      const component = shallow(h(Note, {
+        ...getRequiredProps(),
+        note: {
+          points: [
+            { x: 0, y: 35 },
+            { x: 1, y: 34 },
+          ],
+        },
+      }));
+      const pointConnectorEl = component.find('.note__point-connector');
+      const expected = 'rotate(-44.99999999999999deg) scaleX(56.568542494923804)';
+      expect(pointConnectorEl.prop('style').transform).toEqual(expected);
+    });
+
+    it('should have correct transform applied when the note is a 32nd note', () => {
+      const component = shallow(h(Note, {
+        ...getRequiredProps(),
+        note: {
+          points: [
+            { x: 0, y: 35 },
+            { x: 0, y: 35 },
+          ],
+        },
+      }));
+      const pointConnectorEl = component.find('.note__point-connector');
+      const expected = 'rotate(0deg) scaleX(0)';
+      expect(pointConnectorEl.prop('style').transform).toEqual(expected);
+    });
   });
+
   describe('element __point--end', () => {
-    it('should be defined');
-    it('should have display none when note is 32nd note');
-    it('should have display flex when note is not 32nd note');
-    it('should have translate equal to the changes in x and y between the points multiplied by 40px');
-    it('should invoke mouse down with note and event when mouse down occurs');
-    it('should invoke mouse up with note and event when mouse up occurs');
+    it('should be defined', () => {
+      const component = shallow(h(Note, {
+        ...getRequiredProps(),
+      }));
+      const pointEndEl = component.find('.note__point--end');
+      expect(pointEndEl.length).toEqual(1);
+    });
+
+    it('should have display none when note is 32nd note', () => {
+      const component = shallow(h(Note, {
+        ...getRequiredProps(),
+        note: {
+          points: [
+            { x: 0, y: 35 },
+            { x: 0, y: 35 },
+          ],
+        },
+      }));
+      const pointEndEl = component.find('.note__point--end');
+      expect(pointEndEl.prop('style').display).toEqual('none');
+    });
+
+    it('should have display flex when note is not 32nd note', () => {
+      const component = shallow(h(Note, {
+        ...getRequiredProps(),
+        note: {
+          points: [
+            { x: 0, y: 35 },
+            { x: 1, y: 35 },
+          ],
+        },
+      }));
+      const pointEndEl = component.find('.note__point--end');
+      expect(pointEndEl.prop('style').display).toEqual('flex');
+    });
+
+    it('should have translate equal to the changes in x and y between the points multiplied by 40px', () => {
+      const component = shallow(h(Note, {
+        ...getRequiredProps(),
+        note: {
+          points: [
+            { x: 0, y: 35 },
+            { x: 2, y: 39 },
+          ],
+        },
+      }));
+      const pointEndEl = component.find('.note__point--end');
+      const expected = 'translate(80px, 160px)';
+      expect(pointEndEl.prop('style').transform).toEqual(expected);
+    });
+
+    it('should invoke endpoint mouse down with note and event when mouse down occurs', () => {
+      const note = {
+        points: [
+          { x: 0, y: 35 },
+          { x: 0, y: 35 },
+        ],
+      };
+      const onEndpointMouseDown = sinon.spy();
+      const component = shallow(h(Note, {
+        ...getRequiredProps(),
+        note,
+        onEndpointMouseDown,
+      }));
+      const pointEndEl = component.find('.note__point--end');
+      const e = {};
+      pointEndEl.simulate('mousedown', e);
+      expect(_.last(onEndpointMouseDown.args)[0]).toEqual(note);
+      expect(_.last(onEndpointMouseDown.args)[1]).toEqual(e);
+    });
   });
+
   describe('element __point__fill--end', () => {
-    it('should be defined');
+    it('should be defined', () => {
+      const component = shallow(h(Note, {
+        ...getRequiredProps(),
+      }));
+      const pointFillEndEl = component.find('.note__point__fill--end');
+      expect(pointFillEndEl.length).toEqual(1);
+    });
   });
 });
+
+function getRequiredProps() {
+  return {
+    isSelected: false,
+    note: {
+      points: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+      ],
+    },
+    onEndpointMouseDown: () => {},
+    onMouseDown: () => {},
+    onMouseUp: () => {},
+  };
+}
