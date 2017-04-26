4fea49bc3dce344a5ac18c48de5c861af2d86606
Nick Johnson <nickjohnson.dev@gmail.com>
2017-04-26 18:37:42 -0500

    Removed top-level reference to song saga

--- src/store/saga.js
+++ src/store/saga.js
@@ -3,7 +3,6 @@ import audioClient from '../features/audio-client';
 import sequenceEffects from '../features/sequence-effects';
 import shared from '../features/shared';
 import shortcuts from '../features/shortcuts';
-import song from '../features/song';
 import tracksEffects from '../features/tracks-effects';
 
 export default function* saga() {
@@ -13,7 +12,6 @@ export default function* saga() {
     sequenceEffects.saga(),
     shared.saga(),
     shortcuts.saga(),
-    song.saga(),
     tracksEffects.saga(),
   ];
 }
