51dc7a81ed301c93bbdafb5a8c521a2d08493e80
Nick Johnson <nickjohnson.dev@gmail.com>
2018-01-20 07:31:45 -0600

    Move duplicated files to temp filenames

--- src/features/sequencer/components/slots/__stories__/slots.stories.js
+++
@@ -1,8 +0,0 @@
-import h from 'react-hyperscript';
-import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
-import { SlotsBasics } from './slots-basics.story';
-
-storiesOf('Slots', module)
-  .addDecorator(withKnobs)
-  .add('Basics', () => h(SlotsBasics));
