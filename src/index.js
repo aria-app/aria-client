bdc446f48ef5b473956c701480b723a5c59acc91
Nick Johnson <nickjohnson.dev@gmail.com>
2019-04-26 16:47:09 -0500

    fix: BrowserRouter basename only in prod

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import 'firebase/auth';
-import 'firebase/firestore';
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import app from './features/app';
-import shared from './features/shared';
-import store from './store';
-import './resets.css';
+import "firebase/auth";
+import "firebase/firestore";
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import app from "./features/app";
+import shared from "./features/shared";
+import store from "./store";
+import "./resets.css";
 
 const { AppContainer } = app.components;
 
 store.dispatch(shared.actions.initialized());
 
-render((
-  <BrowserRouter basename="/zen-app">
-    <Provider
-      store={store}>
-        <AppContainer/>
+render(
+  <BrowserRouter
+    basename={process.env.NODE_ENV === "production" ? "/zen-app" : undefined}
+  >
+    <Provider store={store}>
+      <AppContainer />
     </Provider>
-  </BrowserRouter>
-),
-  document.querySelector('#root'),
+  </BrowserRouter>,
+  document.querySelector("#root")
 );
