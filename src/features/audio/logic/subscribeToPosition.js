8ea42a95dd28daaf3114d9df1d075a1ef98c5911
Nick Johnson <nickjohnson.dev@gmail.com>
2019-04-27 21:33:49 -0500

    fix: Dispatch positionChange only when changed

--- src/features/audio/logic/subscribeToPosition.js
+++ src/features/audio/logic/subscribeToPosition.js
@@ -1,14 +1,19 @@
-import { createLogic } from 'redux-logic';
-import shared from '../../shared';
-import dawww from '../dawww';
+import { createLogic } from "redux-logic";
+import * as selectors from "../selectors";
+import shared from "../../shared";
+import dawww from "../dawww";
 
 export const subscribeToPosition = createLogic({
   type: shared.actions.INITIALIZED,
   processOptions: { dispatchMultiple: true },
   warnTimeout: 0,
-  process(args, dispatch) {
-    dawww.onPositionChange((position) => {
+  process({ getState }, dispatch) {
+    dawww.onPositionChange(position => {
+      const prevPosition = selectors.getPosition(getState());
+
+      if (position === prevPosition) return;
+
       dispatch(shared.actions.positionRequestSucceeded(position));
     });
-  },
+  }
 });
