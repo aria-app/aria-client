fa9c33f61898891301283dd36d68c859a4b93c24
Nick Johnson <nickjohnson.dev@gmail.com>
2019-06-14 07:13:51 -0500

    feat: Update some dependencies and use updated primary colors.

--- src/features/shared/theme/index.js
+++ src/features/shared/theme/index.js
@@ -1,7 +1,17 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import teal from '@material-ui/core/colors/teal';
+console.log(teal);
 
 // Default theme reference: https://material-ui.com/customization/default-theme/
 export default createMuiTheme({
+  palette: {
+    primary: {
+      light: '#9b9bf9',
+      main: '#5944ff',
+      dark: '#2f2a9b',
+    },
+    secondary: teal,
+  },
   typography: {
     fontFamily: ['Open Sans', 'Helvetica', 'sans-serif'],
     useNextVariants: true,
