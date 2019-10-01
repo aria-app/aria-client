9a803b1555772d8b2cf16d2e4b17defcb8f23a19
Nick Johnson <nickjohnson.dev@gmail.com>
2019-10-01 17:31:42 -0500

    Update Framer files

--- aria.framerfx/code/canvas.tsx
+++ aria.framerfx/code/canvas.tsx
@@ -1,3 +1,22 @@
 // WARNING: this file is auto generated, any changes will be lost
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const SongListItem = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_TfMtGAJNW", {}, 256,48);
+export const Track = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_bxhCVbQFg", {}, 720,80);
+
+export const colors = Object.freeze({
+    /** #2F2A9B */
+    "PrimaryDark": "var(--token-0f7008a0-ab7a-4a41-8192-a9cc79e4179a, rgb(47, 42, 155))",
+    /** #5944FF */
+    "PrimaryMain": "var(--token-9e3b28b2-1783-4c73-9503-4bd8fd257d5d, rgb(89, 68, 255))",
+    /** #9B9BF9 */
+    "PrimaryLight": "var(--token-bb03464a-5932-4639-bac3-c304fe68e7bf, rgb(155, 155, 249))",
+    /** #FAFAFA */
+    "BackgroundDefault": "var(--token-d0cda82b-c7d2-45f0-90e2-f3c915322df2, rgb(250, 250, 250))",
+    /** #FFFFFF */
+    "BackgroundPaper": "var(--token-9ccac2cb-8047-407b-bfc5-fd4586fee600, rgb(255, 255, 255))",
+    /** rgba(0, 0, 0, 0.12) */
+    "Divider": "var(--token-e98c529a-62d0-474b-aa7c-80f37db4ec14, rgba(0, 0, 0, 0.12))",
+    /** rgba(0, 0, 0, 0.87) */
+    "TextPrimary": "var(--token-a865d633-e1bb-430e-b339-6cf5d7f1e33c, rgba(0, 0, 0, 0.87))",
+})
