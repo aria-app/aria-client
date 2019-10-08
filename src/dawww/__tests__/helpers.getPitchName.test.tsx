82d3d1cf14fa46b88fe98ce4d83f9a1d066d60f2
Nick Johnson <nickjohnson.dev@gmail.com>
2019-10-07 21:22:38 -0500

    Remove test code as it is no longer used.

--- src/dawww/__tests__/helpers.getPitchName.test.tsx
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-import { getPitchName } from '../helpers/getPitchName';
-
-test('should return pitch translated into note name', t => {
-  const expected = 'C3';
-  const result = getPitchName(47);
-  t.is(result, expected);
-});
