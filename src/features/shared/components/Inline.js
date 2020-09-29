86012b2ba806869f723c1543df61523646286870
Nick Johnson <nickjohnson.dev@gmail.com>
2020-09-29 08:56:03 -0500

    Add Inline

---
+++ src/features/shared/components/Inline.js
@@ -0,0 +1,67 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled from 'styled-components';
+
+import { spacingAliases, verticalAlignments } from '../constants';
+import Box from './Box';
+
+const Root = styled.div({
+  display: 'flex',
+});
+
+const Content = styled(Box)(({ align, alignY }) => ({
+  alignItems: {
+    bottom: 'flex-end',
+    center: 'center',
+    stretch: 'stretch',
+    top: 'flex-start',
+  }[alignY],
+  display: 'flex',
+  justifyContent: {
+    center: 'center',
+    left: 'flex-start',
+    right: 'flex-end',
+  }[align],
+  flexWrap: 'wrap',
+}));
+
+const Item = styled(Box)({
+  display: 'flex',
+});
+
+Inline.propTypes = {
+  align: PropTypes.oneOf(['center', 'left', 'right']),
+  alignY: PropTypes.oneOf(verticalAlignments),
+  isReversed: PropTypes.bool,
+  space: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.oneOf(spacingAliases),
+  ]),
+};
+
+export default function Inline(props) {
+  const {
+    align = 'left',
+    alignY = 'top',
+    children,
+    space = 'none',
+    ...rest
+  } = props;
+
+  return (
+    <Root {...rest}>
+      <Content
+        align={align}
+        alignY={alignY}
+        marginLeft={space !== 'none' ? `-${space}` : 'none'}
+        marginTop={space !== 'none' ? `-${space}` : 'none'}
+      >
+        {React.Children.map(children, (child) => (
+          <Item paddingLeft={space} paddingTop={space}>
+            {child}
+          </Item>
+        ))}
+      </Content>
+    </Root>
+  );
+}
---
+++ src/features/shared/components/__stories__/Inline.stories.js
@@ -0,0 +1,52 @@
+import range from 'lodash/fp/range';
+import React from 'react';
+
+import { spacingAliases, verticalAlignments } from '../../constants';
+import Box from '../Box';
+import Inline from '../Inline';
+
+export const InlineDefault = (args) => (
+  <div style={{ maxWidth: 320 }}>
+    <Inline {...args}>
+      {range(0, 10).map((n) => (
+        <Box
+          backgroundColor="subtle"
+          padding={n % 2 === 0 ? 'small' : 'gutter'}
+          key={n}
+        >
+          {n}
+        </Box>
+      ))}
+    </Inline>
+  </div>
+);
+
+export default {
+  title: 'Inline',
+  component: Inline,
+  argTypes: {
+    align: {
+      control: {
+        type: 'inline-radio',
+        options: ['center', 'left', 'right'],
+      },
+    },
+    alignY: {
+      control: {
+        type: 'inline-radio',
+        options: verticalAlignments,
+      },
+    },
+    space: {
+      control: {
+        type: 'select',
+        options: [2.5, ...spacingAliases],
+      },
+    },
+  },
+  args: {
+    align: 'left',
+    alignY: 'stretch',
+    space: 'none',
+  },
+};
