3a8c95d0a9c6935a233f54ceda10edc2007b040b
Nick Johnson <nickjohnson.dev@gmail.com>
2021-07-22 06:57:26 -0500

    Remove unused Apollo hooks after Dashboard conversion

--- src/features/api/hooks/useMe.ts
+++
@@ -1,9 +0,0 @@
-import { QueryHookOptions, QueryResult, useQuery } from '@apollo/client';
-
-import { ME, MeResponse } from '../queries';
-
-type UseMe = (
-  options?: QueryHookOptions<MeResponse, Record<string, never>>,
-) => QueryResult<MeResponse>;
-
-export const useMe: UseMe = (options) => useQuery<MeResponse>(ME, options);
