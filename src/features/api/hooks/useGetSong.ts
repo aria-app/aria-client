538e77cbc40bd3b5ad3b0e587ce827156bfadf2b
Nick Johnson <nickjohnson.dev@gmail.com>
2021-07-26 12:00:28 -0500

    Remove old api hooks

--- src/features/api/hooks/useGetSong.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from '@apollo/client';
-
-import { GET_SONG, GetSongResponse, GetSongVariables } from '../queries';
-import { QueryHook } from './types';
-
-export const useGetSong: QueryHook<GetSongResponse, GetSongVariables> = (
-  options,
-) => useQuery(GET_SONG, options);
